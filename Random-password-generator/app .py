b8cec9187f412ad8e27b21c0b65ed9134137395b
Muhammad Fasi Shahid <l1f22bsse0396@ucp.edu.pk>
2026-05-26 18:19:11 +0500

    Add files via upload
    Developed a Python-based password generator that creates secure random passwords using letters, digits, and special characters. The program utilizes the random and string modules for secure and dynamic password generation based on user-defined length.

---
+++ Random-password-generator/app .py
@@ -0,0 +1,14 @@
+import random
+import string
+
+length = int(input("Enter password length: "))
+
+characters = (
+    string.ascii_letters +
+    string.digits +
+    string.punctuation
+)
+
+password = "".join(random.choice(characters) for i in range(length))
+
+print("Secure Password:", password)
